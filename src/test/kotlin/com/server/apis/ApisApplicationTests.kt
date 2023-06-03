c288b4fd7babba9ede9c2c6c3a01ff72f5d2cc48
Jason (Zheng, Haiqiang) <hqzheng@telenavsoftware.com>
2023-06-03 12:39:52 +0800

    create empty project

---
+++ src/test/kotlin/com/server/apis/ApisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.server.apis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApisApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
